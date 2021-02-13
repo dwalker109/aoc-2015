924722e125557f7c586c1220144f9e007b0858fd
Dan Walker <dwalker109@gmail.com>
2021-02-13 17:43:35 +0000

    Day 5: Doesn't He Have Intern-Elves For This? (cleanup)
    Moved filter functions from closures to fns, general tidying

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -18,101 +18,103 @@ fn get_lines(path: &str) -> Vec<String> {
 }
 
 fn part1(path: &str) -> usize {
-    let lines = get_lines(path);
+    get_lines(path)
+        .into_iter()
+        .filter(has_three_vowels)
+        .filter(is_contiguous_pair)
+        .filter(is_not_banned)
+        .count()
+}
 
-    let processed = lines
-        .iter()
-        .filter(|&line| {
-            let total = line.trim().chars().fold(0, |mut acc, c| {
-                match ['a', 'e', 'i', 'o', 'u'].contains(&c) {
-                    true => acc += 1,
-                    false => (),
-                };
+fn has_three_vowels(line: &String) -> bool {
+    let total = line.trim().chars().fold(0, |mut acc, c| {
+        match ['a', 'e', 'i', 'o', 'u'].contains(&c) {
+            true => acc += 1,
+            false => (),
+        };
 
-                acc
-            });
+        acc
+    });
 
-            total >= 3
-        })
-        .filter(|&line| {
-            let mut last = '_';
-            for c in line.trim().chars() {
-                if c == last {
-                    return true;
-                } else {
-                    last = c;
-                    continue;
-                }
-            }
+    total >= 3
+}
 
-            false
-        })
-        .filter(|&line| {
-            let res = ["ab", "cd", "pq", "xy"].iter().try_for_each(|&banned| {
-                match line.trim().contains(banned) {
-                    true => Err(()),
-                    false => Ok(()),
-                }
-            });
+fn is_contiguous_pair(line: &String) -> bool {
+    let mut last = '_';
+    for c in line.trim().chars() {
+        if c == last {
+            return true;
+        } else {
+            last = c;
+            continue;
+        }
+    }
 
-            res.is_ok()
-        });
+    false
+}
 
-    processed.count()
+fn is_not_banned(line: &String) -> bool {
+    let res = ["ab", "cd", "pq", "xy"].iter().try_for_each(|&banned| {
+        match line.trim().contains(banned) {
+            true => Err(()),
+            false => Ok(()),
+        }
+    });
+
+    res.is_ok()
 }
 
 fn part2(path: &str) -> usize {
-    let lines = get_lines(path);
+    get_lines(path)
+        .into_iter()
+        .filter(is_non_overlapping_pair)
+        .filter(is_char_separated_pair)
+        .count()
+}
 
-    let processed = lines
-        .iter()
-        .filter(|&line| {
-            let indexed_chars = line.chars().enumerate().collect::<Vec<_>>();
-            let pair_windows = indexed_chars.windows(2);
+fn is_non_overlapping_pair(line: &String) -> bool {
+    let indexed_chars = line.chars().enumerate().collect::<Vec<_>>();
+    let pair_windows = indexed_chars.windows(2);
 
-            /* This HashMap is keyed by the pair, and the val contains the index of the prev
-            second letter (to avoid overlaps) and total number of occurrences */
-            let init: HashMap<(char, char), (usize, u32)> = HashMap::new();
+    /* This HashMap is keyed by the pair, and the val contains the index of the prev
+    second letter (to avoid overlaps) and total number of occurrences */
+    let init: HashMap<(char, char), (usize, u32)> = HashMap::new();
 
-            let result = pair_windows.fold(init, |mut acc, el| {
-                let (a_index, a_char) = el[0];
-                let (b_index, b_char) = el[1];
+    let result = pair_windows.fold(init, |mut acc, el| {
+        let (a_index, a_char) = el[0];
+        let (b_index, b_char) = el[1];
 
-                match acc.get_mut(&(a_char, b_char)) {
-                    Some(pair) => {
-                        let (prev_b_index, count) = pair;
-                        if !(a_index == *prev_b_index) {
-                            *pair = (b_index, *count + 1);
-                        }
-                    }
-                    None => {
-                        acc.insert((a_char, b_char), (b_index, 1));
-                    }
+        match acc.get_mut(&(a_char, b_char)) {
+            Some(pair) => {
+                let (prev_b_index, count) = pair;
+                if !(a_index == *prev_b_index) {
+                    *pair = (b_index, *count + 1);
                 }
+            }
+            None => {
+                acc.insert((a_char, b_char), (b_index, 1));
+            }
+        }
 
-                acc
-            });
+        acc
+    });
 
-            result.values().filter(|(_, count)| *count >= 2).count() > 0
-        })
-        .filter(|&line| {
-            let indexed_chars = line.chars().collect::<Vec<_>>();
-            let triplet_windows = indexed_chars.windows(3);
+    result.values().filter(|(_, count)| *count >= 2).count() > 0
+}
 
-            let result = triplet_windows.fold(0, |mut acc, el| {
-                if el[0] == el[2] {
-                    acc += 1;
-                }
+fn is_char_separated_pair(line: &String) -> bool {
+    let indexed_chars = line.chars().collect::<Vec<_>>();
+    let triplet_windows = indexed_chars.windows(3);
 
-                acc
-            });
+    let result = triplet_windows.fold(0, |mut acc, el| {
+        if el[0] == el[2] {
+            acc += 1;
+        }
 
-            result > 0
-        });
+        acc
+    });
 
-    let left = processed.collect::<Vec<_>>();
-
-    left.len()
+    result > 0
 }
 
 #[test]
