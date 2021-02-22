ab38c21ba288b6b337ed2b33979aa04bdad86184
Dan Walker <dwalker109@gmail.com>
2021-02-22 15:17:11 +0000

    Convert day 09 to use custom permutations function rather than itertools (seemed like cheating)

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -3,8 +3,6 @@ use std::{
     fs::read_to_string,
 };
 
-use itertools::Itertools;
-
 fn main() {
     let p1 = part1("./input");
     let p2 = part2("./input");
@@ -14,29 +12,34 @@ fn main() {
 }
 
 fn part1(path: &str) -> u32 {
-    get_routes(path).next().unwrap()
+    *get_routes(path).first().unwrap()
 }
 
 fn part2(path: &str) -> u32 {
-    get_routes(path).last().unwrap()
+    *get_routes(path).last().unwrap()
 }
 
-fn get_routes(path: &str) -> impl Iterator<Item = u32> {
+fn get_routes(path: &str) -> Vec<u32> {
     let data = read_to_string(path).unwrap();
 
     let (locations, distances) = parse_input(&data);
-    let permuations = locations.iter().permutations(locations.len());
+    let permuations = get_permutations(locations.into_iter().collect());
 
-    let routes = permuations.map(|route| {
-        route.windows(2).fold(0, |acc, cities| {
-            let &city_a = cities[0];
-            let &city_b = cities[1];
+    let mut routes = permuations
+        .iter()
+        .map(|route| {
+            route.windows(2).fold(0, |acc, cities| {
+                let city_a = cities[0];
+                let city_b = cities[1];
 
-            acc + *distances.get(&(city_a, city_b)).unwrap()
+                acc + *distances.get(&(city_a, city_b)).unwrap()
+            })
         })
-    });
+        .collect::<Vec<_>>();
 
-    routes.sorted()
+    routes.sort_unstable();
+
+    routes
 }
 
 fn parse_input(raw: &str) -> (HashSet<&str>, HashMap<(&str, &str), u32>) {
@@ -56,6 +59,36 @@ fn parse_input(raw: &str) -> (HashSet<&str>, HashMap<(&str, &str), u32>) {
     }
 
     (locations, distances)
+}
+
+/// This function is completely pointless (I could have used itertools::permutations)
+/// but since this is AoC and I spent ages learning how to do this, I'll use it
+fn get_permutations(all_items: Vec<&str>) -> Vec<Vec<&str>> {
+    let mut collected: Vec<Vec<&str>> = vec![];
+
+    fn recurse<'a, 'b>(
+        collected: &'b mut Vec<Vec<&'a str>>,
+        current: Vec<&'a str>,
+        remaining: Vec<&'a str>,
+    ) {
+        if remaining.len() != 0 {
+            for (i, &next) in remaining.iter().enumerate() {
+                let mut it_remaining = remaining.clone();
+                let mut it_current = current.clone();
+
+                it_remaining.remove(i);
+                it_current.push(next);
+
+                recurse(collected, it_current, it_remaining);
+            }
+        } else {
+            collected.push(current.clone());
+        }
+    }
+
+    recurse(&mut collected, vec![], all_items);
+
+    collected
 }
 
 #[cfg(test)]
