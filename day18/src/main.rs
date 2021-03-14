21953c86034af58cd500f1daf2564d0a8b006306
Dan Walker <dwalker109@gmail.com>
2021-03-14 02:25:30 +0000

    Pointless Rc day 18 experiment

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::read_to_string};
+use std::{borrow::BorrowMut, cell::RefCell, collections::HashMap, fs::read_to_string, rc::Rc};
 
 fn main() {
     let p1 = game_of_life("./input", 100, false);
@@ -23,27 +23,52 @@ fn game_of_life(path: &str, steps: u32, with_stuck: bool) -> usize {
 
 struct Grid<'a> {
     _raw: &'a str,
-    state: HashMap<(isize, isize), bool>,
+    state: HashMap<(isize, isize), Rc<RefCell<bool>>>,
+    neighbours: HashMap<(isize, isize), Vec<Rc<RefCell<bool>>>>,
     stuck: Vec<(isize, isize)>,
 }
 
 impl<'a> Grid<'a> {
     fn from(raw: &str, with_stuck: bool) -> Grid {
-        let mut initial_state = HashMap::new();
+        let y_max = (raw.lines().count() - 1) as isize;
+        let x_max = (raw.lines().next().unwrap().len() - 1) as isize;
+
+        let mut initial_state = HashMap::with_capacity((y_max * x_max) as usize);
 
         for (y, line) in raw.lines().enumerate() {
             for (x, light) in line.chars().enumerate() {
-                initial_state.insert((y as isize, x as isize), light == '#');
+                initial_state.insert(
+                    (y as isize, x as isize),
+                    Rc::from(RefCell::new(light == '#')),
+                );
+            }
+        }
+
+        let mut neighbours = HashMap::with_capacity(((y_max * x_max) as usize) * 8);
+
+        for y in 0..=y_max {
+            for x in 0..=x_max {
+                let vals = (y - 1..=y + 1)
+                    .flat_map(|ny| (x - 1..=x + 1).map(move |nx| (ny, nx)))
+                    .filter(|nyx| nyx != &(y, x))
+                    .map(|nyx| {
+                        Rc::clone(
+                            initial_state
+                                .get(&nyx)
+                                .unwrap_or(&Rc::from(RefCell::new(false))),
+                        )
+                    })
+                    .collect::<Vec<_>>();
+
+                neighbours.insert((y, x), vals);
             }
         }
 
         let stuck = match with_stuck {
             true => {
-                let y_max = (raw.lines().count() - 1) as isize;
-                let x_max = (raw.lines().next().unwrap().len() - 1) as isize;
                 let stuck = vec![(0, 0), (0, x_max), (y_max, 0), (y_max, x_max)];
                 for s in stuck.iter() {
-                    initial_state.insert(*s, true);
+                    initial_state.insert(*s, Rc::from(RefCell::new(true)));
                 }
                 stuck
             }
@@ -55,28 +80,25 @@ impl<'a> Grid<'a> {
         Grid {
             _raw: raw,
             state: initial_state,
+            neighbours,
             stuck,
         }
     }
 
     fn gen_next_state(&mut self) {
-        let mut next_state = self.state.clone();
+        let mut next_state = HashMap::with_capacity(self.state.capacity());
 
         for ((y, x), lit) in self.state.iter() {
-            let lit_neighbours_qty = (y - 1..=y + 1)
-                .flat_map(|ny| (x - 1..=x + 1).map(move |nx| (ny, nx)))
-                .filter(|nyx| nyx != &(*y, *x))
-                .fold(0, |acc, n| {
-                    if let Some(true) = self.state.get(&n) {
-                        acc + 1
-                    } else {
-                        acc
-                    }
-                });
+            let lit_neighbours_qty = self
+                .neighbours
+                .get(&(*y, *x))
+                .unwrap()
+                .iter()
+                .fold(0, |acc, n| if *n.borrow() { acc + 1 } else { acc });
 
             let light_next_state = match lit_neighbours_qty {
-                2 | 3 if *lit => true,
-                3 if !*lit => true,
+                2 | 3 if *lit.borrow() => true,
+                3 if !*lit.borrow() => true,
                 _ => false,
             };
 
@@ -87,11 +109,16 @@ impl<'a> Grid<'a> {
             next_state.insert(*s, true);
         }
 
-        self.state = next_state;
+        for (k, v) in next_state.iter() {
+            self.state.get(k).unwrap().replace(*v);
+        }
     }
 
     fn count_lit_lights(&self) -> usize {
-        self.state.iter().filter(|((_, _), &lit)| lit).count()
+        self.state
+            .iter()
+            .filter(|((_, _), lit)| *lit.borrow())
+            .count()
     }
 }
 
