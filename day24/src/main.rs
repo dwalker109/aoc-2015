3f205998590d325026bd68b3d02ec8b71610fedd
Dan Walker <dwalker109@gmail.com>
2021-04-02 21:54:36 +0100

    "Optimise" day 24 solution to abuse the friendly input data

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -9,28 +9,34 @@ fn main() {
     println!("Part 2: {}", p2);
 }
 
+// This function works with my input but isn't a general solution.
+// It just finds the first (shortest) combination of presents which hits the
+// target - and it just happens to work because the input is friendly!
 fn balance(path: &str, compartments: u64) -> u64 {
     let input = read_to_string(path).unwrap();
     let presents: Vec<u64> = input.lines().map(|n| n.parse().unwrap()).collect();
-
     let target = presents.iter().sum::<u64>() / compartments;
-    let mut groups = Vec::new();
 
-    // Incredibly slow brute force.
     for p in presents.iter().copied().powerset() {
         if p.iter().sum::<u64>() == target {
-            groups.push(p);
+            return p.iter().product();
         }
     }
 
-    // This is a lucky solution, it just abuses the nice input, as
-    // a set of packages which sums correctly is always on the happy path
-    groups
-        .iter()
-        .sorted_by_key(|g| g.iter().copied().product::<u64>())
-        .sorted_by_key(|g| g.len())
-        .next()
-        .unwrap()
-        .iter()
-        .product()
+    panic!("erm...");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(balance("./input", 3), 10723906903);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(balance("./input", 4), 74850409);
+    }
 }
